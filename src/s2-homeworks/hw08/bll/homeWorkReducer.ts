aab21978d24ee885109db3876f726f3b0c3676a0
MarinaArmenovnaDev <awetisyanmarina@gmail.com>
2026-01-26 01:42:57 +0300

    hw3-sprint-3.3

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,21 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
+        case 'sort': {
+            const newState = [...state]
+            if(action.payload === "up"){
+             newState.sort((a,b)=> a.name.localeCompare(b.name))
+            }
+            if(action.payload === "down"){
+                newState.sort((a,b)=> b.name.localeCompare(a.name))
+            }// by name
+            return newState
         }
         case 'check': {
 
-            return state // need to fix
+            return state.filter(el => el.age >=18) // need to fix
         }
         default:
             return state
